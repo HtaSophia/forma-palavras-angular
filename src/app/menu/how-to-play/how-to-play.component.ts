374dca59a7cdbb87331489a3071b40d69ee63d74
Aghata Sophia <aghatasophia@gmail.com>
2021-09-30 20:45:08 -0300

    Create How to Play Component

---
+++ src/app/menu/how-to-play/how-to-play.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-how-to-play',
+    templateUrl: './how-to-play.component.html',
+    styleUrls: ['./how-to-play.component.scss'],
+})
+export class HowToPlayComponent implements OnInit {
+    public ngOnInit(): void {}
+}
