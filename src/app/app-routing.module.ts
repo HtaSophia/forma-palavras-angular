31754e94addca8d4941a2e955fa62180bcec0280
Aghata Sophia <aghatasophia@gmail.com>
2021-09-30 20:51:28 -0300

    Update App Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', redirectTo: '/menu', pathMatch: 'full' },
+    {
+        path: 'menu',
+        loadChildren: async () =>
+            (await import('./menu/menu.module')).MenuModule,
+    },
+    {
+        path: 'levels',
+        loadChildren: async () =>
+            (await import('./level/level.module')).LevelModule,
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
